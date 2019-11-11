5965859005e0db1dba0ca7deed4a37c8c9fa5ff5
Inf1delis <v.mamedov@g.nsu.ru>
2019-11-11 23:42:55 +0700

    Http server

--- echo-server.py
+++ echo-server.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 import redis
-import socket
+# import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 import json
 import os
 
-HOST = ''
-PORT = 65432
+hostName = ''
+hostPort = 65432
 
-prefix = os.environ['PREFIX'] + ':'
-# prefix = 'lol'
+# prefix = os.environ['PREFIX'] + ':'
+prefix = 'lol'
 
 def sendMsg(conn, message):
     message += '\n'
@@ -64,34 +66,144 @@ def server_action(str):
     except:
         return None
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        while True:
-            try:
-                data = conn.recv(1024)
-                json_data = json.loads(data)
-                # sendMsg(conn, 'data received: '+json_data)
-                action = server_action(json_data['action'])
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # self.send_response(200)
+        # self.send_header("Content-type", "text/html")
+        # self.end_headers()
 
-                if action is None:
-                    sendMsg(conn, 'Bad Request')
-                    continue
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
 
-                status = action(json_data)
+        try:
+            json_data = json.loads(post_data)
+            # sendMsg(conn, 'data received: '+json_data)
+            action = server_action(json_data['action'])
 
-                if isinstance(status, tuple):
-                    sendMsg(conn, ('{'+f'"status": "{status[0]}","message": {status[1]}'+'}').replace('\"', '"').replace("'",'"'))
-                else:
-                    sendMsg(conn, json.dumps({
-                        "status": status
-                        }))
-            except:
-                sendMsg(conn, json.dumps({
-                        "status": "Internal Server Error"
-                        }))
+            if action is None or json_data['action'] is "put" \
+                              or json_data['action'] is "delete":
+                self.send_response(400)
+                self.wfile.write(bytes('{'+f'"status": "Bad Request"'+'}', "utf-8"))
+                return
+
+            status = action(json_data)
+
+            self.send_response(200)
+            self.wfile.write(bytes(
+                '{'+f'"status": "{status[0]}","message": {status[1]}'+'}'.replace('\"', '"').replace("'",'"')), "utf-8")
+
+        except:
+            self.send_response(500)
+            self.wfile.write(bytes('{'+f'"status": "Internal Server Error"'+'}', "utf-8"))
+
+
+    def do_POST(self):
+        # self.send_response(200)
+        # self.send_header("Content-type", "text/html")
+        # self.end_headers()
+
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+
+        # print(post_data)
+
+        try:
+            json_data = json.loads(post_data)
+            # sendMsg(conn, 'data received: '+json_data)
+            action = server_action(json_data['action'])
+
+            # print(json_data)
+
+            if action is None:
+                self.send_response(400)
+                self.wfile.write(bytes('{'+f'"status": "Bad Request"'+'}', "utf-8"))
+                return
+
+            # print("put")
+
+            status = action(json_data)
+
+            self.send_response(200)
+            if isinstance(status, tuple):
+                self.wfile.write(('{'+f'"status": "{status[0]}","message": {status[1]}'+'}')
+                    .replace('\"', '"')
+                    .replace("'",'"')
+                    .encode("utf-8"))
+            else:
+                self.wfile.write(bytes('{'+f'"status": "{status}"'+'}', "utf-8"))
+
+        except:
+            self.send_response(500)
+            self.wfile.write(bytes('{'+f'"status": "Internal Server Error"'+'}', "utf-8"))
+
+
+    def do_DELETE(self):
+        # self.send_response(200)
+        # self.send_header("Content-type", "text/html")
+        # self.end_headers()
+
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+
+        try:
+            json_data = json.loads(post_data)
+            # sendMsg(conn, 'data received: '+json_data)
+            action = server_action(json_data['action'])
+
+            if action is None or json_data['action'] is "get" \
+                              or json_data['action'] is "put":
+                self.send_response(400)
+                self.wfile.write(bytes('{'+f'"status": "Bad Request"'+'}', "utf-8"))
+                return
+
+            status = action(json_data)
+
+            self.send_response(200)
+            self.wfile.write(bytes('{'+f'"status": "{status}"'+'}', "utf-8"))
+
+
+        except:
+            self.send_response(500)
+            self.wfile.write(bytes('{'+f'"status": "Internal Server Error"'+'}', "utf-8"))
+
+
+
+
+myServer = HTTPServer((hostName, hostPort), MyServer)
+print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
+print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+
+    # with conn:
+    #     while True:
+    #         try:
+    #             data = conn.recv(1024)
+    #             json_data = json.loads(data)
+    #             # sendMsg(conn, 'data received: '+json_data)
+    #             action = server_action(json_data['action'])
+
+    #             if action is None:
+    #                 sendMsg(conn, 'Bad Request')
+    #                 continue
+
+    #             status = action(json_data)
+
+    #             if isinstance(status, tuple):
+    #                 sendMsg(conn, ('{'+f'"status": "{status[0]}","message": {status[1]}'+'}').replace('\"', '"').replace("'",'"'))
+    #             else:
+    #                 sendMsg(conn, json.dumps({
+    #                     "status": status
+    #                     }))
+    #         except:
+    #             sendMsg(conn, json.dumps({
+    #                     "status": "Internal Server Error"
+    #                     }))
 
 
 
